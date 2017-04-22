da91017ac04ac2fddfb8f294b2e6992753929002
Maizeee <aztecorn@gmail.com>
2017-04-23 04:49:27 +1200

    title page created and objects

--- src/game/menu/ExitButtonObject.java
+++ src/game/menu/ExitButtonObject.java
@@ -2,19 +2,27 @@ package game.menu;
 
 import core.Asset;
 import core.AssetHandler;
+import core.Game;
 import core.GameObject;
+import game.Button;
+import game.TestLevel;
 
 /**
  * Created by zva on 22/04/17.
  */
-public class ExitButtonObject extends GameObject {
+public class ExitButtonObject extends Button {
 
+    private float width;
+    private float height;
 
     private Asset asset;
 
     public ExitButtonObject(float x, float y) {
         position.x = x;
         position.y = y;
+        width = 378;
+        height = 81;
+
 
         asset = AssetHandler.getAsset("exitbutton.png");
     }
@@ -25,8 +33,14 @@ public class ExitButtonObject extends GameObject {
         return asset;
     }
 
-    @Override
-    public void update(float dt) {
 
+
+    @Override
+    public void onClick(Game game, int x, int y) {
+        if (x > position.x && x < position.x + width &&
+                y > position.y && y < position.y + height) {
+            
+            game.exit();
+        }
     }
 }
--- src/game/menu/PlayButtonObject.java
+++ src/game/menu/PlayButtonObject.java
@@ -2,26 +2,40 @@ package game.menu;
 
 import core.Asset;
 import core.AssetHandler;
+import core.Game;
 import core.GameObject;
 import core.components.Collidable;
 import core.components.ColliderComponent;
 import core.components.Physicable;
 import core.components.PhysicsComponent;
+import game.Button;
+import game.TestLevel;
 
 /**
  * Created by zva on 22/04/17.
  */
-public class PlayButtonObject extends GameObject {
+public class PlayButtonObject extends Button  {
 
 
     private Asset asset;
 
+    private float width;
+    private float height;
+
     public PlayButtonObject(float x, float y) {
         position.x = x;
         position.y = y;
+        width = 378;
+        height = 81;
+
 
         asset = AssetHandler.getAsset("playbutton.png");
+
+
+
     }
+
+
 
 
     @Override
@@ -29,8 +43,15 @@ public class PlayButtonObject extends GameObject {
         return asset;
     }
 
-    @Override
-    public void update(float dt) {
 
+    @Override
+    public void onClick(Game game, int x, int y) {
+        if (x > position.x && x < position.x + width &&
+                y > position.y && y < position.y + height) {
+            game.changeLevel(new TestLevel());
+        }
     }
+
+
+
 }
